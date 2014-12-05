2dd7c9a1676b05ad9b076f63e0f9fd9cba69430d
Craig <chelmann@psrc.org>
2014-12-04 16:08:58 -0800

    May 5th 2014 - Batch file cleanup

--- batchfiles/deletefiles/delete_files.bat
+++
@@ -1,6 +0,0 @@
-REM call from root folder (e.g., PSRC_TDModel_1200/2006)
-REM deletes matrix, report, indicator files from most folders
-REM useful for prepping a completed model run for compression and archiving
-call batchfiles/deletefiles/delete_matrix_files.bat
-call batchfiles/deletefiles/delete_report_files.bat
-call batchfiles/deletefiles/delete_indicator_files.bat
---
+++ batchfiles/deletefiles/delete_matrix_files_notg.bat
@@ -0,0 +1,236 @@
+REM deletes *.emx (matrix) files from all emmemat folders
+REM auto assignments
+cd assignments/auto/am/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd assignments/auto/md/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd assignments/auto/pm/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd assignments/auto/ev/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd assignments/auto/ni/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM non-motorized assignments
+cd assignments/nonmotorized/am/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM transit assignments
+cd assignments/transit/am/all_mode/emmemat
+if exist *.emx erase *.emx
+cd ../../../../..
+cd assignments/transit/md/all_mode/emmemat
+if exist *.emx erase *.emx
+cd ../../../../..
+REM distribution
+cd distribution/hbcollege/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/hbother/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/hbschool/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/hbshop/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/hbwork_inc1/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/hbwork_inc2/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/hbwork_inc3/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/hbwork_inc4/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/nhbother/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd distribution/nhbwork/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+REM trip generation
+REM cd generation/trip/emmemat
+REM if exist *.emx erase *.emx
+REM cd ../../..
+REM market segmentation
+cd marketsegmentation/nonwork/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd marketsegmentation/work/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+REM mode choice
+cd modechoice/hbcollege/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd modechoice/hbnonwork/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd modechoice/hbschool/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd modechoice/hbwork_inc1/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd modechoice/hbwork_inc2/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd modechoice/hbwork_inc3/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd modechoice/hbwork_inc4/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+cd modechoice/nhb/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+REM networks
+cd networks/emmemat
+if exist *.emx erase *.emx
+cd ../..
+REM auto skims
+cd skims/auto/am/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/auto/md/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/auto/pm/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/auto/ev/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/auto/ni/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM bidirectional skims
+cd skims/bidirectional/distance/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/bidirectional/time/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/bidirectional/generalized_cost/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM intrazonal/terminal times
+cd skims/iz_tt/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+REM non-motorized skims
+cd skims/nonmotorized/am/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM park and ride skims
+cd skims/pnr/income1/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/pnr/income2/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/pnr/income3/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd skims/pnr/income4/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM transit skims
+cd skims/transit/am/all_mode/emmemat
+if exist *.emx erase *.emx
+cd ../../../../..
+cd skims/transit/md/all_mode/emmemat
+if exist *.emx erase *.emx
+cd ../../../../..
+REM time of day
+cd timeofday/delay/allpurposes/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd timeofday/hbother/hov/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd timeofday/hbother/sov/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd timeofday/hbshop/hov/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd timeofday/hbshop/sov/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd timeofday/hbwork/hov/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd timeofday/hbwork/sov/income1/emmemat
+if exist *.emx erase *.emx
+cd ../../../../..
+cd timeofday/hbwork/sov/income2/emmemat
+if exist *.emx erase *.emx
+cd ../../../../..
+cd timeofday/hbwork/sov/income3/emmemat
+if exist *.emx erase *.emx
+cd ../../../../..
+cd timeofday/hbwork/sov/income4/emmemat
+if exist *.emx erase *.emx
+cd ../../../../..
+REM auto trip tables
+cd triptables/autos/am/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd triptables/autos/md/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd triptables/autos/pm/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd triptables/autos/ev/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd triptables/autos/ni/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM non-motorized trip tables
+cd triptables/nonmotorized/all/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM park and ride trip tables
+cd triptables/parkandride/income1/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd triptables/parkandride/income2/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd triptables/parkandride/income3/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+cd triptables/parkandride/income4/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM transit walk-access trip tables
+cd triptables/transit/walkaccess/emmemat
+if exist *.emx erase *.emx
+cd ../../../..
+REM truck trip tables
+cd triptables/trucks/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+REM truck model
+cd trucks/distribution/emmemat
+if exist *.emx erase *.emx
+cd ../../..
+REM cd trucks/generation/emmemat
+REM if exist *.emx erase *.emx
+REM cd ../../..
+REM vehicle availability
+cd vehicleavailability/all/emmemat
+if exist *.emx erase *.emx
+cd ../../..
