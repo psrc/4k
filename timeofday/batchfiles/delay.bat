dcd2d11bb3f010bc73316a0faa2a8548f921732b
Craig Helmann <CHelmann@psrc.org>
2018-03-20 13:32:51 -0700

    Combined Home Based Shopping and Home Based Other Distribution

--- timeofday/batchfiles/delay.bat
+++ timeofday/batchfiles/delay.bat
@@ -4,9 +4,4 @@ REM Created by PSRC staff
 REM Calculates the Generalized Cost Delay by Time of Day
 REM Free-Flow is considered the overnight period
 
-cd allpurposes
-call emme -ng 000 -m macros\1_initialize_matrices.mac
-call emme -ng 000 -m macros\2_initialize_full_matrices.mac
-call emme -ng 000 -m macros\3_import_matrices.mac
-call emme -ng 000 -m macros\4_calculate_delay.mac
-cd ..
+call emme -ng 000 -m macros\1_calculate_delay.mac
--- timeofday/batchfiles/tod_hov.bat
+++ timeofday/batchfiles/tod_hov.bat
@@ -5,22 +5,12 @@ REM Runs the Time of Day Choice for the HOV Modes for HBShop, HBO and HBW
 
 cd hov
 
-REM Initialize the Bank
-call emme -ng 000 -m macros\1_initialize_matrices.mac
-call emme -ng 000 -m macros\2_initialize_origin_matrices.mac
-call emme -ng 000 -m macros\3_initialize_destination_matrices.mac
-call emme -ng 000 -m macros\4_initialize_full_matrices.mac
-call emme -ng 000 -m macros\5_import_data.mac
-call emme -ng 000 -m macros\6_calculate_hh_size.mac
-call emme -ng 000 -m macros\7_import_matrices.mac
-call emme -ng 000 -m macros\8_calculate_adjusted_delay.mac
-call emme -ng 000 -m macros\9_calculate_adjusted_bridge_flag.mac
-call emme -ng 000 -m macros\10_calculate_hbwork_hov_a-p_tod_shares.mac
-call emme -ng 000 -m macros\11_calculate_hbwork_hov_p-a_tod_shares.mac
-call emme -ng 000 -m macros\12_calculate_hbshop_hov_a-p_tod_shares.mac
-call emme -ng 000 -m macros\13_calculate_hbshop_hov_p-a_tod_shares.mac
-call emme -ng 000 -m macros\14_calculate_hbother_hov_a-p_tod_shares.mac
-call emme -ng 000 -m macros\15_calculate_hbother_hov_p-a_tod_shares.mac
+REM Run Time of Day for HOV trips
+call emme -ng 000 -m macros\1_initialization.mac
+call emme -ng 000 -m macros\2_calculate_hbwork_hov_a-p_tod_shares.mac
+call emme -ng 000 -m macros\3_calculate_hbwork_hov_p-a_tod_shares.mac
+call emme -ng 000 -m macros\4_calculate_hbother_hov_a-p_tod_shares.mac
+call emme -ng 000 -m macros\5_calculate_hbother_hov_p-a_tod_shares.mac
 
 REM write indicator file
 cd ..
